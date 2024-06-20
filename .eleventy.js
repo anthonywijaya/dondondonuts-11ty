34450e0797a57dc9a8a0b5fa1567ef3f55aebfcc
Anthony Wijaya <anthony.fengshen@gmail.com>
2024-06-20 19:03:37 +0700

    first content

---
+++ .eleventy.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const htmlmin = require("html-minifier");
+
+module.exports = function(eleventyConfig) {
+
+  if (process.env.ELEVENTY_PRODUCTION) {
+    eleventyConfig.addTransform("htmlmin", htmlminTransform);
+  }
+
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
+
+  // Passthrough
+  // eleventyConfig.addPassthroughCopy({ "src/static": "." });
+  eleventyConfig.addPassthroughCopy({"src/_includes/assets/img/**/*": "assets/img/"});
+
+  // Watch targets
+  eleventyConfig.addWatchTarget("./src/_includes/assets/css/");
+  
+
+  var pathPrefix = "";
+  if (process.env.GITHUB_REPOSITORY) {
+    pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
+  }
+
+  return {
+    dir: {
+      input: "src"
+    },
+    pathPrefix
+  }
+};
+
+function htmlminTransform(content, outputPath) {
+  if( outputPath.endsWith(".html") ) {
+    let minified = htmlmin.minify(content, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true
+    });
+    return minified;
+  }
+  return content;
+}
---
+++ src/_data/site.js
@@ -0,0 +1,6 @@
+module.exports = {
+    title: 'Don Don Donuts',
+    description: 'Ube Donuts. Based in Sunter, Jakarta.',
+    keywords: ['donuts', "donut", 'ubedon', "sunter", "indonesia"],
+    url: 'https://dondondonuts.id', // your site url without trailing slash
+  };
