2aa4df61dabfb20f72cab3b83e64f98de8435280
Anthony Wijaya <anthony.fengshen@gmail.com>
2024-07-04 21:15:14 +0700

    adds sitemap + robot

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: 'Don Don Donuts',
     description: 'Ube Donuts. Based in Sunter, Jakarta.',
-    keywords: ['donuts', "donut", 'ubedon', "sunter", "indonesia"],
+    keywords: ['dondondonuts', 'dondon donuts', 'donuts', "donut", 'ubedon', "sunter", "indonesia"],
     url: 'https://dondondonuts.id', // your site url without trailing slash
   };
