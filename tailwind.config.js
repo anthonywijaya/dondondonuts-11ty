d33ec9d85c7de09458547d1e3aa599ff95613064
Anthony Wijaya <anthony.fengshen@gmail.com>
2024-11-01 19:07:43 +0700

    blocks out 3th nov and adds snow effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,8 @@ module.exports = {
         pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         fadeIn: 'fadeIn 0.8s ease-out forwards', // Add the new animation
         bounce: 'bounce 2s infinite',
+        'bounce-gentle': 'bounce-gentle 3s infinite',
+        'text-reveal': 'text-reveal 0.5s ease-out forwards',
       },
       keyframes: {
         pulse: {
@@ -55,6 +57,26 @@ module.exports = {
             transform: 'translateY(20px)',
           },
           to: {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'bounce-gentle': {
+          '0%, 100%': {
+            transform: 'translateY(-5%)',
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+        'text-reveal': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(1em)',
+          },
+          '100%': {
             opacity: '1',
             transform: 'translateY(0)',
           },
